df9946187fad3ed1087a700d1842597e0a92228f
Ashhad06 <129995285+Ashhad06@users.noreply.github.com>
2023-07-22 01:08:37 +0500

    Add files via upload

---
+++ Practice2.py
@@ -0,0 +1,9 @@
+import random
+A= ["ashhad","omar","osman", "ahmed","abdullah","hassan","shebaz","huzaifa","abbas"]
+print(A)
+C= random.choice(A)
+B= input("Enter your guess?")
+if random.choice==B:
+    print("Congratulations! You got that right")
+else:
+    print("Sorry better luck next time", "The right answer was",C)
